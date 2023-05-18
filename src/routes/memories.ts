7138047af00a49905bc5f7809202cc126f5013d8
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-05-18 02:05:08 -0300

    get unique memorie

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -19,7 +19,21 @@ export async function memoriesRoutes(app: FastifyInstance) {
     })
   })
 
-  app.get('/memories/:id', async (req) => {})
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
+  })
 
   app.post('/memories', async (req) => {})
 
