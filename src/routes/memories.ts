5e34df97aeb46806ea9f199f3b59899c6d8e1032
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-05-18 02:06:19 -0300

    delete memory

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -85,5 +85,17 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory
   })
 
-  app.delete('/memories/:id', async () => {})
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
+  })
 }
