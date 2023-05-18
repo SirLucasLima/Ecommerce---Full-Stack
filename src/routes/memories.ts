056cd90c694a08c395b1427cabc551133af67e8a
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-05-18 02:05:26 -0300

    create memorie

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -35,7 +35,26 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory
   })
 
-  app.post('/memories', async (req) => {})
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: '5ac8562f-5c67-49da-8319-5646cf8b8b6d',
+      },
+    })
+
+    return memory
+  })
 
   app.put('/memories/:id', async () => {})
 
