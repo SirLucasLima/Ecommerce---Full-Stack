8d76cb66028528955f8c6f7e2207c65304e12dac
JoviTech <developerlucaslima@gmail.com>
2023-05-19 02:32:44 -0300

    githubId & jwt

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
@@ -8,6 +10,12 @@ const app = fastify()
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
